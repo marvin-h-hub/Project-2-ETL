09e70306203968f4a919294cbaab3d242882204a
marvin-h-hub <60294426+marvin-h-hub@users.noreply.github.com>
2020-04-29 18:57:15 -0400

    Add files via upload

---
+++ ETL Project/2queriesproject.sql
@@ -0,0 +1,8 @@
+-- Database: Test
+
+-- DROP DATABASE "Test";
+
+Select * from public."Team_Alpha";
+
+SELECT "Country", "Happiness Rank" from public."Team_Alpha";
+SELECT "Internet Percent Usage Rank", "Internet and Happiness Score" from public."Team_Alpha";
